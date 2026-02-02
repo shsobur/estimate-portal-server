88cc41fb58983333291b0156aa7081a6be6e36b2
unknown <shsoburhossen951@gmail.com>
2026-02-02 21:22:59 +0600

    Server basic setup completed

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware__
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get("/", (req, res) => {
+  res.json({ message: "MERN Server is running!" });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
