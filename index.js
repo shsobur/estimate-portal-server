01027fbeaf6911cafc4d7b65d6008a3701887346
unknown <shsoburhossen951@gmail.com>
2026-02-21 16:04:51 +0600

    Changed db connection string

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 dotenv.config();
 
@@ -10,6 +11,43 @@ const PORT = process.env.PORT || 5000;
 // Middleware__
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster-01-512m.ch5skz9.mongodb.net/?appName=Cluster-01-512M`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    // API routes would go here example__
+    app.get("/users", async (req, res) => {
+      const result = await client
+        .db("test")
+        .collection("users")
+        .find({})
+        .toArray();
+      res.json(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Basic route
 app.get("/", (req, res) => {
